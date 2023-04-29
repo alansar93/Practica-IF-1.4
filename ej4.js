052ef19fa2853132bc6b26f2d879503eb1cea0c7
Alan Sarchman <alansarchman@live.com.ar>
2023-04-29 20:42:24 -0300

    actualizado y mejorado

--- ej4.js
+++ ej4.js
@@ -1,4 +1,4 @@
-const INGREDIENTE1 = "carne";
+/* const INGREDIENTE1 = "carne";
 const INGREDIENTE2 = "pollo";
 
 
@@ -16,4 +16,23 @@ if ((tipoSopa == INGREDIENTE1) && (hayVerdura == "si") && (hayAgua == "si") && (
     document.write ("El robot le a preparado una sopa de " + tipoSopa + " con caldo");
 }
 
+ */
 
+const titulo = document.querySelector("#titulo");
+const ingrediente1 = document.querySelector("#ingrediente1");
+const ingrediente2 = document.querySelector("#ingrediente2");
+const ingrediente3 = document.querySelector("#ingrediente3");
+const ingrediente4 = document.querySelector("#ingrediente4");
+const ingrediente5 = document.querySelector("#ingrediente5");
+const ingrediente6 = document.querySelector("#ingrediente6");
+const robot = document.querySelector("#robot");
+
+titulo.innerHTML = "Ingrese V para afirmativo, F para falso";
+
+function sopa() {
+if (((ingrediente1.value == "v") && (ingrediente2.value == "v")) && ((ingrediente3.value == "v") || (ingrediente4.value == "v")) && ((ingrediente5.value == "v") || (ingrediente6.value == "v"))) {
+    robot.innerHTML = "EL ROBOT PUEDE PREARARLO"
+} else { robot.innerHTML = "EL ROBOT NO PUEDE PREPARARLO"
+    
+}
+}
